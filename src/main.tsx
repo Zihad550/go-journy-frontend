63fd3198176c3dabeacfe94b43b0a6b13e22ca90
Zihad550 <dev.jehadhossain@gmail.com>
2025-08-23 09:28:37 +0600

    setup: react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider as ReduxProvider } from "react-redux";
-import App from "./App.tsx";
+import { RouterProvider } from "react-router";
 import { Toaster } from "./components/ui/sonner.tsx";
 import "./index.css";
 import { store } from "./redux/store.ts";
+import { router } from "./routes/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ReduxProvider store={store}>
-      <App />
+      <RouterProvider router={router} />
       <Toaster />
     </ReduxProvider>
   </StrictMode>,
