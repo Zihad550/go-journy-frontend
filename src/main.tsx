59bb91dee861f396a73c4ca7a99a1603cca7ee23
Zihad550 <dev.jehadhossain@gmail.com>
2025-08-23 09:40:03 +0600

    setup: gsap animation library

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider as ReduxProvider } from "react-redux";
@@ -6,6 +8,8 @@ import { Toaster } from "./components/ui/sonner.tsx";
 import "./index.css";
 import { store } from "./redux/store.ts";
 import { router } from "./routes/index.tsx";
+
+gsap.registerPlugin(useGSAP); // register the hook to avoid React version discrepancies
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
