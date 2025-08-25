c2c094895d09c9d5a47ae1695146fce4240ef535
Zihad550 <dev.jehadhossain@gmail.com>
2025-08-25 10:02:43 +0600

    feat: add public routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,36 @@
 import App from "@/App";
+import About from "@/pages/About";
+import Contact from "@/pages/Contact";
+import FAQ from "@/pages/FAQ";
+import Features from "@/pages/Features";
+import Home from "@/pages/Home";
 import { createBrowserRouter } from "react-router";
 
 export const router = createBrowserRouter([
   {
     Component: App,
     path: "/",
+    children: [
+      {
+        Component: Home,
+        index: true,
+      },
+      {
+        Component: About,
+        path: "about",
+      },
+      {
+        Component: Features,
+        path: "features",
+      },
+      {
+        Component: Contact,
+        path: "contact",
+      },
+      {
+        Component: FAQ,
+        path: "faq",
+      },
+    ],
   },
 ]);
